107e7d41cc4e248574aeca2afbd4b08aba02a246
William Whitaker <billydwhitaker@gmail.com>
2020-08-18 09:39:11 -0400

    conditional links and captions for images on the front page

--- front-page.php
+++ front-page.php
@@ -9,12 +9,17 @@
  ?>
 
 <main id="primary" class="site-main">
-  <?php
-  while ( have_posts() ) :
-    the_post();
-    get_template_part( 'template-parts/content', get_post_type() );
-  endwhile;
-  ?>
+  <section class="site-main-section">
+    <h2 class="front_page-content-title">
+      recent work
+    </h2>
+    <?php
+    while ( have_posts() ) :
+      the_post();
+      get_template_part( 'template-parts/content', get_post_type() );
+    endwhile;
+    ?>
+  </section>
 </main>
 
  <?php
--- template-parts/content-wwa_artwork.php
+++ template-parts/content-wwa_artwork.php
@@ -8,19 +8,40 @@
  ?>
 
  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div class="entry_content">
-    <?php $image = wp_get_attachment_image_src( get_field('image'), 'large'); ?>
-    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image'))?>" />
+  <div class="entry-content">
+    <?php $image = wp_get_attachment_image_src( get_field('image'), 'large'); 
+    if ( is_singular() ) : ?>
+    <a href="<?php echo get_home_url() ?>">
+      <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image'))?>" />
+    </a>
     <?php
-    if ( is_singular() ) :
-      the_title( '<h1 class="entry-title">', '</h1>' );
-    else :
-      the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+    else : ?>
+    <a href="<?php echo esc_url( get_permalink() ) ?>">
+      <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image'))?>" />
+    </a>
+    <?php
     endif;
     ?>
-    <p><?php the_field('medium'); ?></p>
-    <p><?php the_field('date'); ?></p>
-    <p><?php the_field('dimensions'); ?></p>
-  </div>
+    <div class="entry-caption">
+      <?php
+      if ( is_singular() ) :
+        the_title( '<h2 class="entry-title">', '</h2>' );
+      ?>
+        <p class="entry-field">
+          <?php the_field( 'medium' ); ?>
+        </p>
+        <p class="entry-field">
+          <?php the_field( 'date' ); ?>
+        </p>
+        <p class="entry-field">
+          <? the_field( 'dimensions' ); ?>
+        </p>
+      <?php
+      else :
+        the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+      endif;
+      ?>
+    </div>
+  </div><!--.entry-content -->
 
  </article>
