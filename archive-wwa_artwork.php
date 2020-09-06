897c37b334d355fe7ae1692d9f7b47da7160b040
William Whitaker <billydwhitaker@gmail.com>
2020-09-06 06:04:22 -0400

    refactors wwa_archive.php

--- archive-wwa_artwork.php
+++ archive-wwa_artwork.php
@@ -9,19 +9,15 @@
  ?>
 
 <main id="primary" class="site-main">
-  <section class="site-main-section">
-    <h2 class="site-main-section-header">
-      Artwork
-    </h2>
-    <div class="site-main-section-content">
-      <?php
-      while ( have_posts() ) :
-        the_post();
-        get_template_part( 'template-parts/content', 'wwa_artwork' );
-      endwhile;
-      ?>
-    </div><!-- site-main-section-content -->
-  </section>
+<?php get_template_part(
+    'template-parts/content',
+    'section',
+    array(
+      'sectionTitle' => 'Artwork',
+      'templatePart' => 'wwa_artwork'
+    )
+  );
+  ?>
 </main>
 
  <?php get_footer(); ?>
