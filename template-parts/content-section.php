d67973cab2d3cb6400c9d09ccd0f8eb414bbdf06
William Whitaker <billydwhitaker@gmail.com>
2020-09-07 20:44:29 -0400

    excludes about page from getting section header

--- template-parts/content-section.php
+++ template-parts/content-section.php
@@ -1,9 +1,12 @@
 <section class="site-main-section">
+  <?php if ( !( is_page( 'about' ) ) ) : ?>
   <header class="site-main-section-header">
     <h2>
       <?php echo $args['sectionTitle']; ?>
     </h2>
   </header><!-- site-main-section-header -->
+  <?php endif ?>
+
   <div class="site-main-section-content"> 
     <?php
       while ( have_posts() ) :
