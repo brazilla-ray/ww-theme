b254c9c924ec2d89f1d98cfbd7471327a1c083cc
William Whitaker <billydwhitaker@gmail.com>
2020-09-09 21:51:06 -0400

    cleans up wwa_artwork styling

--- template-parts/content-front.php
+++ template-parts/content-front.php
@@ -7,17 +7,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div class="entry-content">
+  <div class="<?php echo get_post_type()?>__image">
     <?php $image = wp_get_attachment_image_src( get_field('image'), 'large'); ?>
     <a href="<?php echo esc_url( get_permalink() ) ?>">
       <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image'))?>" />
     </a>
-  </div><!--.entry-content -->
-  <div class="entry-caption">
+  </div><!--.*__image -->
+  <div class="<?php echo get_post_type()?>__caption">
     <?php
       the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
     ?>
-  </div><!-- entry-caption -->
- 
-
+  </div><!-- .*__caption -->
  </article>
--- template-parts/content-section.php
+++ template-parts/content-section.php
@@ -1,18 +1,18 @@
-<section class="site-main-section">
+<section class="main-section">
   <?php if ( !( is_page( 'about' ) ) ) : ?>
-  <header class="site-main-section-header">
-    <h2>
+  <header class="main-section-title">
+    <h2 class="main-section-title__heading">
       <?php echo $args['sectionTitle']; ?>
     </h2>
-  </header><!-- site-main-section-header -->
+  </header><!-- main-section__header -->
   <?php endif ?>
 
-  <div class="site-main-section-content"> 
+  <div class="main-section-content"> 
     <?php
       while ( have_posts() ) :
         the_post();
         get_template_part( 'template-parts/content', $args[templatePart] );
       endwhile;
     ?>
-  </div><!--- site-main-section-content -->
-</section><!-- site-main-section -->
+  </div><!--- main-section__content -->
+</section><!-- main-section -->
