8ffeba33bf2f41a404e20e5612d893c368fcba84
William Whitaker <billydwhitaker@gmail.com>
2020-09-29 18:53:36 -0400

    changes to content-page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,13 +8,10 @@
  */
 
 ?>
-
-<section class="main-section">
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="article-content">
-			<?php
-			the_content()
-			?>
-		</div><!-- .article-content -->
-	</article><!-- #post-<?php the_ID(); ?> -->
-</section>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="article-content">
+		<?php
+		the_content()
+		?>
+	</div><!-- .article-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
