6442e92daeb4a5faedfa2002b89127ff7449390c
William Whitaker <billydwhitaker@gmail.com>
2020-12-03 14:59:42 -0500

    creates content-post

--- index.php
+++ index.php
@@ -13,40 +13,11 @@
  */
 
 get_header();
-?>
-
-<main id="primary" class="site-main">
-
-<?php 
-// if the post type is builtin, show the content.
-// otherwise, modify the query to get a custom post type (but which one?)
-if ( have_posts() ) : 
-	while ( have_posts() ) : the_post(); 
-	endwhile;
-else :
-	_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-endif; 
-?>
-<pre>
-	<?php
-	$args = array(
-       'public'   => true,
-       '_builtin' => false,
-    );
-
-    $output = 'names'; // names or objects, note names is the default
-    $operator = 'and'; // 'and' or 'or'
-
-    $post_types = get_post_types( $args, $output, $operator ); 
-
-    foreach ( $post_types  as $post_type ) {
-
-       echo '<p>' . $post_type . '</p>';
-    }
-
-    ?>
-</pre>
-</main><!-- #main -->
-
-<?php
+get_template_part( 
+	'template-parts/content',
+	'main',
+	array (
+		'templatePart' => get_post_type()
+	)
+);
 get_footer();
