75fa56413135e774a53ec53247acb810ec426816
William Whitaker <billydwhitaker@gmail.com>
2020-12-03 15:14:36 -0500

    improves conditional titles in main content

--- template-parts/content-main.php
+++ template-parts/content-main.php
@@ -9,13 +9,17 @@
 <main id="primary" class="site-main">
   <section class="main-section">
     <header class="main-section-title">
+      <h2 class="main-section-title__heading">
       <?php
-      if ( ! $args['sectionTitle'] == '' ) {
-        echo '<h2 class="main-section-title__heading">' . $args['sectionTitle'] . '</h2>';
-      }
-        ?>
+        if ( ! $args['sectionTitle'] == '' ) :
+          echo $args['sectionTitle'];
+        else :
+          echo the_title();
+        endif;  
+      ?>
+      </h2> 
     </header>
-    <?php
+      <?php 
       // The Loop.
       if ( have_posts() ) : 
         while ( have_posts() ) : the_post();
