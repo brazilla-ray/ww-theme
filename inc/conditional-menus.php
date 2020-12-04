fe9001f79faed8562d401a83a391d25f0240e4a9
William Whitaker <billydwhitaker@gmail.com>
2020-12-04 10:23:04 -0500

    aligns code (formatting)

--- inc/conditional-menus.php
+++ inc/conditional-menus.php
@@ -2,11 +2,11 @@
 
 function wwa_archive_menu( $menu ) {
   wp_nav_menu( array(
-    'menu' => $menu,
-    'container' => 'nav',
+    'menu'            => $menu,
+    'container'       => 'nav',
     'container_class' => 'artwork-navigation',
-    'menu_class' => 'artwork-menu',
-    'add_li_class' => 'artwork-menu__item'
+    'menu_class'      => 'artwork-menu',
+    'add_li_class'    => 'artwork-menu__item'
   ) );
 }
 // add_filter( 'wp_nav_menu_args', 'wwa_conditional_menus' );
--- inc/custom-menus.php
+++ inc/custom-menus.php
@@ -3,12 +3,12 @@ function register_ww_menus() {
   register_nav_menus(
     array(
       'navigation' => esc_html__( 'Primary', 'ww-theme' ),
-      'artwork' => esc_html__( 'Artwork', 'ww-theme' ),
-      'paintings' => esc_html__( 'Paintings', 'ww-theme' ),
-      'gouache' => esc_html__( 'Gouache', 'ww-theme' ),
-      'acrylic' => esc_html__( 'Acrylic', 'ww-theme' ),
-      'drawing' => esc_html__( 'Drawing', 'ww-theme' ),
-      'collage' => esc_html__( 'Collage', 'ww-theme' )
+      'artwork'    => esc_html__( 'Artwork', 'ww-theme' ),
+      'paintings'  => esc_html__( 'Paintings', 'ww-theme' ),
+      'gouache'    => esc_html__( 'Gouache', 'ww-theme' ),
+      'acrylic'    => esc_html__( 'Acrylic', 'ww-theme' ),
+      'drawing'    => esc_html__( 'Drawing', 'ww-theme' ),
+      'collage'    => esc_html__( 'Collage', 'ww-theme' )
     )
   );
 }
